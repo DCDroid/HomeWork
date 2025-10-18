98f3e0983f3396aff1166b9f1df8000171163b71
DCDroid <dddroid6@gmail.com>
2025-10-18 06:56:33 +0000

    DB - 05 end

--- 6 - SQL/05/Users/src/Users.cpp
+++ 6 - SQL/05/Users/src/Users.cpp
@@ -13,16 +13,16 @@ Users::~Users()
 void Users::connect_to_db()
 {
     _db_connection = std::make_unique<pqxx::connection>(
-        "host=localhost user= password= dbname=users port=5432"
+        "host=localhost user=this_is_my_user password=this_is_my_password dbname=users port=5432"
     );
 
     // Проверка, было ли соединение успешно установлено
     if (!_db_connection || !_db_connection->is_open())
     {
-        std::cerr << "Connection to DB failed!" << std::endl;
-        return;
+        throw std::runtime_error("DB connection is not open.");
     }
 
+    
     _db_connection->prepare("insert_user", "INSERT INTO Users (email, name, second_name) VALUES ($1, $2, $3)");
 
     _db_connection->prepare("insert_phone_number", "INSERT INTO PhoneNumbers (email, phone_number) VALUES ($1, $2)");
@@ -35,6 +35,7 @@ void Users::connect_to_db()
 
     _db_connection->prepare("delete_user", "DELETE FROM Users WHERE email = $1");
 
+
     std::cout << _db_connection->is_open() << std::endl;
 }
 
@@ -42,8 +43,7 @@ void Users::create_db()
 {
     if(!_db_connection || !_db_connection->is_open())
     {
-        std::cerr << "DB connection is not open." << std::endl;
-        return;
+        throw std::runtime_error("DB connection is not open.");
     }
     pqxx::work tx( *_db_connection );
     tx.exec(
@@ -68,41 +68,25 @@ void Users::add_user(UserInfo& user_info)
 {
     if (!_db_connection || !_db_connection->is_open())
     {
-        std::cerr << "DB connection is not open." << std::endl;
-        return;
+        throw std::runtime_error("DB connection is not open.");
     }
-    try
-    {
-        pqxx::work tx( *_db_connection );
-        tx.exec_prepared("insert_user", user_info.email, user_info.name, user_info.second_name);
-        for(auto i : user_info.phone_numbers)
-            tx.exec_prepared("insert_phone_number", user_info.email, i);
-        tx.commit();
-    }
-    catch(pqxx::sql_error e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    pqxx::work tx( *_db_connection );
+    tx.exec_prepared("insert_user", user_info.email, user_info.name, user_info.second_name);
+    for(auto i : user_info.phone_numbers)
+        tx.exec_prepared("insert_phone_number", user_info.email, i);
+    tx.commit();
 }
 
 void Users::add_phone_number(UserInfo& user_info)
 {
     if (!_db_connection || !_db_connection->is_open())
     {
-        std::cerr << "DB connection is not open." << std::endl;
-        return;
+        throw std::runtime_error("DB connection is not open.");
     }
-    try
-    {
-        pqxx::work tx( *_db_connection );
-        for(auto i : user_info.phone_numbers)
-            tx.exec_prepared("insert_phone_number", user_info.email, i);
-        tx.commit();
-    }
-    catch(pqxx::sql_error e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    pqxx::work tx( *_db_connection );
+    for(auto i : user_info.phone_numbers)
+        tx.exec_prepared("insert_phone_number", user_info.email, i);
+    tx.commit();
 }   
 
 // Удаляет ВСЕ старые номера телефонов пользователя, если предлагается хотя бы 1 новый номер телефона
@@ -110,88 +94,59 @@ void Users::change_user(UserInfo& info)
 {
     if (!_db_connection || !_db_connection->is_open())
     {
-        std::cerr << "DB connection is not open." << std::endl;
-        return;
+        throw std::runtime_error("DB connection is not open.");
     }
-
-    try
+    pqxx::work tx( *_db_connection );
+    if(info.name.empty() == false)
     {
-        pqxx::work tx( *_db_connection );
-
-        if(info.name.empty() == false)
-        {
-            tx.exec_prepared("update_user_name", info.name, info.email);
-        }
-        
-        if(info.second_name.empty() == false)
-        {
-            tx.exec_prepared("update_user_second_name", info.second_name, info.email);
-        }
-
-        if(info.phone_numbers.empty() == false)
-        {
-            // Удаляем ВСЕ старые номера
-            tx.exec_prepared("delete_user_phones", info.email);
-            
-            // Добавляем новые
-            for(const auto& phone : info.phone_numbers)
-            {
-                tx.exec_prepared("insert_phone_number", info.email, phone);
-            }
-        }
-        
-        tx.commit();
+        tx.exec_prepared("update_user_name", info.name, info.email);
     }
-    catch(const pqxx::sql_error& e)
+    
+    if(info.second_name.empty() == false)
     {
-        std::cerr << e.what() << '\n';
+        tx.exec_prepared("update_user_second_name", info.second_name, info.email);
     }
+    if(info.phone_numbers.empty() == false)
+    {
+        // Удаляем ВСЕ старые номера
+        tx.exec_prepared("delete_user_phones", info.email);
+        
+        // Добавляем новые
+        for(const auto& phone : info.phone_numbers)
+        {
+            tx.exec_prepared("insert_phone_number", info.email, phone);
+        }
+    }
+    
+    tx.commit();
 }
 
 void Users::delete_phone_number(UserInfo& info)
 {
     if (!_db_connection || !_db_connection->is_open())
     {
-        std::cerr << "DB connection is not open." << std::endl;
-        return;
+        throw std::runtime_error("DB connection is not open.");
     }
-
-    try
+    pqxx::work tx( *_db_connection );
+    for(const auto& phone : info.phone_numbers)
     {
-        pqxx::work tx( *_db_connection );
-        for(const auto& phone : info.phone_numbers)
-        {
-            tx.exec_params("DELETE FROM PhoneNumbers WHERE email = $1 AND phone_number = $2", info.email, phone);
-        }
-        tx.commit();
+        tx.exec_params("DELETE FROM PhoneNumbers WHERE email = $1 AND phone_number = $2", info.email, phone);
     }
-    catch(const pqxx::sql_error& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    tx.commit();
 }
 
 void Users::delete_user(std::string email)
 {
     if (!_db_connection || !_db_connection->is_open())
     {
-        std::cerr << "DB connection is not open." << std::endl;
-        return;
+        throw std::runtime_error("DB connection is not open."); 
     }
-
-    try
-    {
-        pqxx::work tx( *_db_connection );
-        // Сначала удаляем связанные номера телефонов из-за FOREIGN KEY
-        tx.exec_prepared("delete_user_phones", email);
-        // Затем удаляем самого пользователя
-        tx.exec_prepared("delete_user", email);
-        tx.commit();
-    }
-    catch(const pqxx::sql_error& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    pqxx::work tx( *_db_connection );
+    // Сначала удаляем связанные номера телефонов из-за FOREIGN KEY
+    tx.exec_prepared("delete_user_phones", email);
+    // Затем удаляем самого пользователя
+    tx.exec_prepared("delete_user", email);
+    tx.commit();
 }
 
 UserInfo Users::find_user(std::string email)
@@ -199,33 +154,23 @@ UserInfo Users::find_user(std::string email)
     UserInfo user;
     if (!_db_connection || !_db_connection->is_open())
     {
-        std::cerr << "DB connection is not open." << std::endl;
-        return user;
+        throw std::runtime_error("DB connection is not open.");
     }
-
-    try
+    pqxx::nontransaction ntx( *_db_connection );
+    
+    // Поиск информации о пользователе
+    pqxx::result user_res = ntx.exec_params("SELECT name, second_name FROM Users WHERE email = $1", email);
+    if (!user_res.empty())
     {
-        pqxx::nontransaction ntx( *_db_connection );
-        
-        // Поиск информации о пользователе
-        pqxx::result user_res = ntx.exec_params("SELECT name, second_name FROM Users WHERE email = $1", email);
-        if (!user_res.empty())
+        user.email = email;
+        user.name = user_res[0][0].as<std::string>();
+        user.second_name = user_res[0][1].as<std::string>();
+        // Поиск номеров телефонов
+        pqxx::result phones_res = ntx.exec_params("SELECT phone_number FROM PhoneNumbers WHERE email = $1", email);
+        for (const auto& row : phones_res)
         {
-            user.email = email;
-            user.name = user_res[0][0].as<std::string>();
-            user.second_name = user_res[0][1].as<std::string>();
-
-            // Поиск номеров телефонов
-            pqxx::result phones_res = ntx.exec_params("SELECT phone_number FROM PhoneNumbers WHERE email = $1", email);
-            for (const auto& row : phones_res)
-            {
-                user.phone_numbers.push_back(row[0].as<std::string>());
-            }
+            user.phone_numbers.push_back(row[0].as<std::string>());
         }
-    }
-    catch(const pqxx::sql_error& e)
-    {
-        std::cerr << e.what() << '\n';
     }
     
     return user;
--- 6 - SQL/05/main.cpp
+++ 6 - SQL/05/main.cpp
@@ -2,15 +2,26 @@
 #include "Users.h"
 #include <pqxx/pqxx>
 
-
 int main()
 {   
     Users users;
 
-    users.connect_to_db();
-    users.create_db();
+    try
+    {
+        users.connect_to_db();
+        users.create_db();
+    }
+    catch(const pqxx::sql_error& e)
+    {
+        std::cerr << e.what() << '\n';  
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
     
+    try
     {
         UserInfo user;
         user.email = "jimhalpert@dunder-mifflin.com";
@@ -20,9 +31,16 @@ int main()
 
         users.add_user(user);
     }
+    catch(const pqxx::sql_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << e.what() << '\n';  
+    }
 
-    /*
-
+    try
     {
         UserInfo user;
         user.email = "jimhalpert@dunder-mifflin.com";
@@ -30,8 +48,16 @@ int main()
         user.phone_numbers.push_back("(502) 142-9867");
         users.add_phone_number(user);
     }
+    catch(const pqxx::sql_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << e.what() << '\n';  
+    }
     
-
+    try
     {
         UserInfo user;
         user.email = "jimhalpert@dunder-mifflin.com";
@@ -40,24 +66,47 @@ int main()
         user.phone_numbers.push_back("(502) 111-2222");
         users.change_user(user);
     }
-
+    catch(const pqxx::sql_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << e.what() << '\n';  
+    }
     
-
+    try 
     {
         UserInfo user;
         user.email = "jimhalpert@dunder-mifflin.com";
         user.phone_numbers.push_back("(502) 111-2222"); // номер, который нужно удалить
         users.delete_phone_number(user);
     }
-        
-    */
-
+    catch(const pqxx::sql_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << e.what() << '\n';  
+    }    
     
+    /*
+    try
     {   
         users.delete_user("jimhalpert@dunder-mifflin.com");
+    }   
+    catch(const pqxx::sql_error& e)
+    {
+        std::cerr << e.what() << '\n';
     }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << e.what() << '\n';  
+    }    
+    */
     
-    
+    try
     {
         auto user = users.find_user("jimhalpert@dunder-mifflin.com");
         std::cout << "Name: " << user.name << "\nSecond Name: " << user.second_name << "\nEmail: " << user.email << "\nPhone Numbers:\n";
@@ -65,6 +114,14 @@ int main()
         {
             std::cout << phone << std::endl;
         }
+    }   
+    catch(const pqxx::sql_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << e.what() << '\n';  
     }
 
     return 0;
