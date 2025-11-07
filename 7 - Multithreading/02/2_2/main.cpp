a6fc7b678b1120d1d78cd4e89d8bd689a1f3ca6e
DCDroid <dddroid6@gmail.com>
2025-11-07 11:46:42 +0000

    7.2 Progress bar fixed

--- 7 - Multithreading/02/2_2/main.cpp
+++ 7 - Multithreading/02/2_2/main.cpp
@@ -9,15 +9,24 @@
 #include <mutex>
 #include <atomic>
 
-// std::mutex mtx;
-std::atomic<uint32_t> count_of_ended_threads = 0;
+#define NUNBER_POSSITION 1
+#define ID_POSSITION 10
+#define PROGRESS_BAR_POSSITION 30
+#define TOTAL_TIME_POSSITION 58
+
+void setCursorPosition(int x, int y) {
+    std::cout << "\033[" << y << ";" << x << "H"; //  << std::flush;
+}
+
+std::mutex mtx;
+// std::atomic<uint32_t> count_of_ended_threads = 0;
 
 struct thread_info
 {
     uint32_t thread_number;
-    std::thread::id thread_id;
-    std::atomic<uint32_t> progress = 0;
-    std::chrono::milliseconds duration{0};
+    std::atomic<std::thread::id> thread_id;
+    std::atomic<uint32_t> progress{0};
+    std::atomic<long long> duration_ms{0};
 };
 
 struct Node
@@ -28,52 +37,47 @@ struct Node
 
 void Node::foo()
 {
-    m_info.thread_id = std::this_thread::get_id();
+    m_info.thread_id.store(std::this_thread::get_id());
+    
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        setCursorPosition(NUNBER_POSSITION, m_info.thread_number + 2);
+        std::cout << m_info.thread_number << std::flush;
+        setCursorPosition(ID_POSSITION, m_info.thread_number + 2);
+        std::cout << m_info.thread_id.load() << std::flush;
+    }
 
     auto t0 = std::chrono::steady_clock::now();
 
     for (int i = 0; i < 100; ++i) {
         uint32_t sleep_time = static_cast<uint32_t>(std::rand() % 100);
         std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
-        m_info.progress = (i + 1) / 100.0 * 30;
+        m_info.progress = (i + 1) / 100.0 * 20;
+
+        // тут нужно захватить мьютекс на консоль и вывести прогресс бар
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            setCursorPosition(PROGRESS_BAR_POSSITION + m_info.progress, m_info.thread_number + 2);
+            std::cout << 'X' << std::flush;
+        }
     }
 
-    count_of_ended_threads++;
-
     auto t1 = std::chrono::steady_clock::now();
-    m_info.duration = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
-}
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+    m_info.duration_ms.store(duration.count());
 
-void bar_generator(std::vector<Node> &nodes)
-{
-    while(count_of_ended_threads < nodes.size())
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-        std::system("clear");
-        std::cout << "# \t id \t\t\t\t  Progress Bar \t\t\t    Time \n";
-        for (const auto &n : nodes) {
-            std::cout << n.m_info.thread_number << " \t " << n.m_info.thread_id << " \t "; 
-            for(int i = 0; i < 30; ++i) {
-                if(i < n.m_info.progress) {
-                    std::cout << "#";
-                } else {
-                    std::cout << "-";
-                }
-            }
-
-            if(n.m_info.progress >= 30) {
-                std::cout << " \t " << n.m_info.duration.count() << " ms\n";
-            } else {
-                std::cout << " \t In Process\n";
-            }
-        }
-        
+        std::lock_guard<std::mutex> lock(mtx);
+        setCursorPosition(TOTAL_TIME_POSSITION, m_info.thread_number + 2);
+        std::cout << m_info.duration_ms.load() << " ms" << std::flush;
     }
 }
 
 int main()
 {
+    std::system("clear");
+    std::cout << "# \t id \t\t\t  Progress Bar \t\t Time \n\n\n\n";
+
     const uint32_t thread_count = 4;
 
     std::vector<std::thread> threads;
@@ -85,15 +89,12 @@ int main()
         threads.emplace_back(&Node::foo, &nodes[i]);
     }
 
-    std::thread bat_thread(bar_generator, std::ref(nodes));
-    bat_thread.join();
-
     for(auto& t : threads)
     {
         t.join();
     }
 
-    
+    setCursorPosition(0, thread_count + 2);
     
     return 0;
 }
